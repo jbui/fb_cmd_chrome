8ca3ced421fb171a327a352cc0b44096a0c2c32c
James <j3bui@ucsd.edu>
2012-03-03 06:33:58 -0800

    beeperbox

--- contentscript.js
+++ contentscript.js
@@ -21,25 +21,28 @@
       var value = el[0].value;
       var post_url = 'http://www.google.com';
 
-      jQuery('body').append('<div id="fbcmd-notice"></div>');
+      jquery('body').append('<div id="BeeperBox" class="UIBeeper viewportBottom viewportLeft viewportRight"><div class="UIBeeper_Full"><div class="Beeps"><div class="UIBeep UIBeep_Top UIBeep_Bottom" style="opacity: 1; "><a class="UIBeep_NonIntentional" href="www.google.com"><div class="UIBeep_Icon"><img class="beeper_icon img" src="http://static.ak.fbcdn.net/rsrc.php/v1/yT/r/4QVMqOjUhcd.gif" alt="" width="16" height="16"></div><span class="beeper_x">&nbsp;</span><div id="fbcmd-notice" class="UIBeep_Title"><span class="blueName">fb_cmp</span></div></a></div></div></div></div>');
+
+
+      // jQuery('body').append('<div id="fbcmd-notice"></div>');
       jQuery('#fbcmd-notice').hide();
-      jQuery('#fbcmd-notice').css('z-index', '150000');
-      jQuery('#fbcmd-notice').css('position', 'absolute');
-      jQuery('#fbcmd-notice').css('top', '60px');
-      jQuery('#fbcmd-notice').css('width', '100%');
-      jQuery('#fbcmd-notice').css({'margin': 'auto'});
-      jQuery('#fbcmd-notice').css({'color': 'black', 'font-size': '1.6em'});
-      jQuery('#fbcmd-notice').css('background-color', 'rgba(175,175,175,0.8)');
-      jQuery('#fbcmd-notice').css('text-align', 'center');
+      // jQuery('#fbcmd-notice').css('z-index', '150000');
+      // jQuery('#fbcmd-notice').css('position', 'absolute');
+      // jQuery('#fbcmd-notice').css('top', '60px');
+      // jQuery('#fbcmd-notice').css('width', '100%');
+      // jQuery('#fbcmd-notice').css({'margin': 'auto'});
+      // jQuery('#fbcmd-notice').css({'color': 'black', 'font-size': '1.6em'});
+      // jQuery('#fbcmd-notice').css('background-color', 'rgba(175,175,175,0.8)');
+      // jQuery('#fbcmd-notice').css('text-align', 'center');
       jQuery('#fbcmd-notice').html('Processing ... Please Wait ...');
       jQuery('#fbcmd-notice').fadeIn('fast')
       chrome.extension.sendRequest({user: user, value: value}, function(response) {
         console.log(response);
         jQuery('#fbcmd-notice').html('Success! Go to <a href="' +
                               'http://www.facebook.com/me">http://www.facebook.com/me</a> to see the result.');
-        jQuery('#fbcmd-notice').css({'color': 'green', 'font-size': '1.6em'});
-        jQuery('#fbcmd-notice').css('background-color', 'rgba(51,255,153,0.8)');
-        setTimeout("jQuery('#fbcmd-notice').fadeOut('fast')", 3000);
+        // jQuery('#fbcmd-notice').css({'color': 'green', 'font-size': '1.6em'});
+        // jQuery('#fbcmd-notice').css('background-color', 'rgba(51,255,153,0.8)');
+        setTimeout("jQuery('#BeeperBox').fadeOut('fast')", 3000);
 
 
       });
