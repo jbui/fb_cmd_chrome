9c9f86aa4c28ee58b2f7c99f2ebfd8f98fa153d7
lewisf <lewis.f.chung@gmail.com>
2012-03-03 13:39:07 -0800

    final 2

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,11 @@
   console.log("Loaded extension");
-  
-  //var el = jQuery('.attachmentName').first();      // Update Status
-  //el.bind("click", initiateSexyTime);
 
-  //function initiateSexyTime() {
-  var el = jQuery('[name=xhpc_message]');
-  el.on('keydown', processText);
-  //}
-  // Ajax this shit to our server!
+  jQuery('[name=xhpc_message]').on('focus', function(e) {
+    jQuery(e.target).on('keydown', self.processText);
+  });
+  jQuery('[name=xhpc_message]').on('blur', function(e) {
+    jQuery(e.target).off('keydown', self.processText);
+  });
 
   function processText(e) {
     console.log("Process text");
@@ -24,23 +22,24 @@
       jQuery('body').append('<div id="fbcmd-notice"></div>');
       jQuery('#fbcmd-notice').hide();
       jQuery('#fbcmd-notice').css('z-index', '150000');
-      jQuery('#fbcmd-notice').css('position', 'absolute');
-      jQuery('#fbcmd-notice').css('top', '80px');
-      jQuery('#fbcmd-notice').css('width', '100%');
-      jQuery('#fbcmd-notice').css('line-height', '120px');
-      jQuery('#fbcmd-notice').css({'margin': 'auto'});
+      jQuery('#fbcmd-notice').css('position', 'fixed');
+      jQuery('#fbcmd-notice').css('top', '60px');
+      jQuery('#fbcmd-notice').css('left', '60px');
+      jQuery('#fbcmd-notice').css('width', '500px');
+      jQuery('#fbcmd-notice').css('line-height', '80px');
       jQuery('#fbcmd-notice').css({'color': 'black', 'font-size': '1.6em'});
       jQuery('#fbcmd-notice').css('background-color', 'rgba(175,175,175,0.8)');
+      jQuery('#fbcmd-notice').css('border', '1px solid black');
       jQuery('#fbcmd-notice').css('text-align', 'center');
-      jQuery('#fbcmd-notice').html('Processing ... Please Wait ...');
+      jQuery('#fbcmd-notice').html('Processing...');
       jQuery('#fbcmd-notice').fadeIn('fast')
       chrome.extension.sendRequest({user: user, value: value}, function(response) {
         console.log(response);
-        jQuery('#fbcmd-notice').html('Success! Click <a href="' +
-                              'http://www.facebook.com/me">here</a> to continue.');
+        jQuery('#fbcmd-notice').html('Done!');
         jQuery('#fbcmd-notice').css({'color': 'green', 'font-size': '1.6em'});
+        jQuery('#fbcmd-notice').css('border', '1px solid green');
         jQuery('#fbcmd-notice').css('background-color', 'rgba(51,255,153,0.8)');
-        setTimeout("jQuery('#fbcmd-notice').fadeOut('fast')", 9000);
+        setTimeout("jQuery('#fbcmd-notice').fadeOut('fast')", 3000);
 
 
       });
